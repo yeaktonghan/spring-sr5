201ee72e38d82706cdbb3a2fd00ce7f634ab5d31
yeaktonghan <yeaktonghan.github@gmail.com>
2023-08-08 16:19:06 +0700

    add testing

--- src/main/java/com/setec/sr5/controller/HomeController.java
+++ src/main/java/com/setec/sr5/controller/HomeController.java
@@ -2,11 +2,17 @@ package com.setec.sr5.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
     }
 }
